9c8821c422bee937fad52811b1cc6af3b98185c3
Akingbade675 <akingbadebasit26@gmail.com>
2023-05-19 23:30:29 +0000

    implemented LFU cache replacement algorithm

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -20,26 +20,38 @@ class LFUCache(BaseCaching):
     def put(self, key, item):
         '''assign to the dictionary self.cache_data
         the item value for the key key'''
-        if key is not None and item is not None:
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
 
-            # adding or modifying an item means it is the most recently used
-            # the item is added to the end of the queue
-            if key in self.queue:
-                index = self.queue.index(key)
-                self.queue.pop(index)
-            self.queue.append(key)
-            self.freq_table[key] = self.freq_table[key] + 1 
-                    if self.freq_table.get(key) else 1
+        self.cache_data[key] = item
 
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                # the least recently used item is at the front of the queue
-                # the the first item is popped
-                freq = sorted(self.freq_table.items(), key=lambda x: x[1])
-                key = self.queue.pop(freq[0][1] if freq[0][1] != freq[1][1] else 0)
-                del self.cache_data[key]
-                del self.freq_table[key]
-                print('DISCARD: {}'.format(key))
+        # check if cache is full
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # the least recently used item is at the front of the queue
+            # the the first item is popped
+
+            # Find the least frequency used items
+            min_frequency = min(self.freq_table.values())
+            least_frequent_keys = [
+                                    k for k, v in self.freq_table.items()
+                                    if v == min_frequency]
+
+            # If there are multiple least frequent items, use LRU algorithm
+            lru_key = self.queue[
+                                min([self.queue.index(key)
+                                    for key in least_frequent_keys])]
+            del self.cache_data[lru_key]
+            del self.freq_table[lru_key]
+            print('DISCARD: {}'.format(lru_key))
+
+        # adding or modifying an item means it is the most recently used
+        # the item is added to the end of the queue
+        if key in self.queue:
+            index = self.queue.index(key)
+            self.queue.pop(index)
+        self.queue.append(key)
+        self.freq_table[key] = (self.freq_table[key] + 1
+                                if self.freq_table.get(key) else 1)
 
     def get(self, key):
         '''Returns: the value in self.cache_data linked to key'''
@@ -52,5 +64,6 @@ class LFUCache(BaseCaching):
         index = self.queue.index(key)  # gets the index of the MRU item
         self.queue.pop(index)
         self.queue.append(key)
+        self.freq_table[key] += 1
 
         return value
